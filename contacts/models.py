036e1855d6d8e74479510709745304431dc424ef
mnpenchev <mnpenchev@gmail.com>
2025-07-22 16:36:14 +0000

    chore(models): added tests for models

--- contacts/models.py
+++ contacts/models.py
@@ -4,7 +4,7 @@ from django.db.models import CASCADE
 
 class TestContact(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, blank=False, null=False)
     created_at =models.DateTimeField(auto_now_add=True)
 
 
